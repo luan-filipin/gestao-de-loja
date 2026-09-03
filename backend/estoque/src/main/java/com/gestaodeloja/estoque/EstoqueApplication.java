3dcd719006a685eea269fc0a74464e45ea0eb765
luan-filipin <luan.filipin@hotmail.com>
2026-09-03 09:50:42 -0300

    - Restruturação do projeto.

--- backend/estoque/src/main/java/com/gestaodeloja/estoque/EstoqueApplication.java
+++ backend/estoque/src/main/java/com/gestaodeloja/estoque/EstoqueApplication.java
@@ -1,13 +1,13 @@
-package com.gestaodeloja.backend;
+package com.gestaodeloja.estoque;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class EstoqueApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BackendApplication.class, args);
+        SpringApplication.run(EstoqueApplication.class, args);
     }
 
 }
