55f430d2c824dd3b714b0e6bf9dd80de97707145
luan-filipin <luan.filipin@hotmail.com>
2026-09-10 13:59:46 -0300

    - Incluido a pasta .idea no gitignore - Removido application-test.yaml pois nao estava sendo usado. - Implementado a anotação @Import na class EstoqueApplicationTests.java

--- backend/estoque/src/test/java/com/gestaodeloja/estoque/EstoqueApplicationTests.java
+++ backend/estoque/src/test/java/com/gestaodeloja/estoque/EstoqueApplicationTests.java
@@ -1,9 +1,12 @@
 package com.gestaodeloja.estoque;
 
+import com.gestaodeloja.estoque.config.PostgresTestContainer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(PostgresTestContainer.class)
 class EstoqueApplicationTests {
 
     @Test
