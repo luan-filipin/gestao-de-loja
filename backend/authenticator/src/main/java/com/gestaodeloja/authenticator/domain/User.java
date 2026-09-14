3b9e85ba5d53a9601c16595da230d8a5d572e5f9
luan-filipin <luan.filipin@hotmail.com>
2026-09-14 12:16:53 -0300

    - Implementado novos campos a domain User. - Corrigido teste existentes. - Corrigido xml do liquibase.

--- backend/authenticator/src/main/java/com/gestaodeloja/authenticator/domain/User.java
+++ backend/authenticator/src/main/java/com/gestaodeloja/authenticator/domain/User.java
@@ -5,11 +5,14 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.jspecify.annotations.NullMarked;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
 
@@ -35,6 +38,18 @@ public class User implements UserDetails {
     @Enumerated(EnumType.STRING)
     private UserRole role;
 
+    @Builder.Default
+    @Column(nullable = false)
+    private boolean enabled = true;
+
+    @Column(nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime created;
+
+    @Column(nullable = false)
+    @UpdateTimestamp
+    private LocalDateTime updated;
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
@@ -48,5 +63,10 @@ public class User implements UserDetails {
     @Override
     public String getUsername() {
         return this.userName;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.enabled;
     }
 }
--- backend/authenticator/src/test/java/com/gestaodeloja/authenticator/fixture/UserFixture.java
+++ backend/authenticator/src/test/java/com/gestaodeloja/authenticator/fixture/UserFixture.java
@@ -3,18 +3,26 @@ package com.gestaodeloja.authenticator.fixture;
 import com.gestaodeloja.authenticator.domain.User;
 import com.gestaodeloja.authenticator.domain.enums.UserRole;
 
+import java.time.LocalDateTime;
+
 public class UserFixture {
 
     public static User criaUser(
             Long id,
             String userName,
             String password,
-            String role) {
+            UserRole role,
+            boolean enabled,
+            LocalDateTime created,
+            LocalDateTime updated) {
         return User.builder()
                 .id(id)
                 .userName(userName)
                 .password(password)
-                .role(UserRole.valueOf(role))
+                .role(role)
+                .enabled(enabled)
+                .created(created)
+                .updated(updated)
                 .build();
     }
 }
